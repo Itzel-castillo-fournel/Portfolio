0265b3adc8a02a30b4451054aefa320f32e889b9
Itzel-castillo-fournel <libitzel.castillo@gmail.com>
2025-04-07 11:43:22 +0200

    modif engagement

--- password.js
+++ password.js
@@ -1,16 +1,29 @@
 // Fonction pour vérifier le mot de passe et afficher l'image
 function checkPassword() {
     const password = document.getElementById('password-input').value;
-    const correctPassword = "bjgvebes#211sfrz"; // Définissez votre mot de passe ici
+    const correctPassword = ""; 
     
     if (password === correctPassword) {
-        // Afficher l'image et masquer le formulaire de mot de passe
+        // Affiche l'image et masquer le formulaire de mot de passe
         document.getElementById('password-form').style.display = 'none';
         document.getElementById('protected-image').style.display = 'block';
         
-        // Charger l'image après validation du mot de passe
+        // Charge l'image après validation du mot de passe
         document.getElementById('protected-image').src = 'Images/SI.png';
     } else {
         alert('Mot de passe incorrect. Veuillez réessayer.');
     }
 }
+
+function checkPasswordEngagement() {
+    var password = document.getElementById("password-input").value;
+    const correctPassword = "";
+
+    if (password === correctPassword) {
+        document.getElementById("protected-content").style.display = "block";
+        document.getElementById("password-form").style.display = "none";
+    } else {
+        alert("Mot de passe incorrect. Veuillez réessayer.");
+    }
+}
+
